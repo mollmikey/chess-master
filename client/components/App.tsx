79f01712a1544fe6f81154b98403a72e7a4243dd
Michael Moll <michael.moll@developersinstitute.co.nz>
2024-04-08 13:06:24 +1200

    Sidebar corrected

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,6 +1,6 @@
 import { Outlet, Link } from 'react-router-dom'
 
-const chessboardImg = '/images/empty-chessboard.svg'
+const chessboardImg = '/images/chessboard.svg'
 const practiceImg = '/images/chess-pawn.svg'
 const piecesImg = '/images/chess.svg'
 const settingsImg = '/images/settings-svgrepo-com.svg'
@@ -20,24 +20,24 @@ function App() {
         </Link>
         <div className="sidebar">
           <Link to="/" className="board-link">
-            <img className="board-logo" src={chessboardImg} alt="Board"></img>
             Board
+            <img className="board-logo" src={chessboardImg} alt="Board"></img>
           </Link>
           <Link to="/practice" className="practice-link">
-            <img className="practice-logo" src={practiceImg} alt="Practice" />
             Practice
+            <img className="practice-logo" src={practiceImg} alt="Practice" />
           </Link>
           <Link to="/pieces" className="pieces-link">
-            <img className="pieces-logo" src={piecesImg} alt="Pieces" />
             Pieces
+            <img className="pieces-logo" src={piecesImg} alt="Pieces" />
           </Link>
           <Link to="/settings" className="settings-link">
-            <img className="settings-logo" src={settingsImg} alt="Settings" />
             Settings
+            <img className="settings-logo" src={settingsImg} alt="Settings" />
           </Link>
           <Link to="/user" className="user-link">
-            <img className="user-logo" src={userImg} alt="User Profile" />
             User
+            <img className="user-logo" src={userImg} alt="User Profile" />
           </Link>
         </div>
         <div className="content">
